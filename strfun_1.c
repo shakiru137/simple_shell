492aa760509482b75959692285b48b09cd6273ab
midunodunsi <midun99.odunsi@gmail.com>
2024-01-05 15:01:43 -0800

    reupdated

--- strfun_1.c
+++ strfun_1.c
@@ -7,17 +7,21 @@
  *
  * Return: pointer to destination
  */
+
+/* function to copy the contents of one string to another */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
+	/* Check if the source and destination strings are the same or if the source is NUL */
 	if (dest == src || src == 0)
-		return (dest);
+		return (dest); /* If NULL, return the destination string without copying anything */
 	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	/* Add a null terminator to the destination string to ensure it is properly terminated */
 	dest[i] = 0;
 	return (dest);
 }
@@ -28,6 +32,8 @@ char *_strcpy(char *dest, char *src)
  *
  * Return: pointer to the duplicated string
  */
+
+/* function to duplicate a string and return a pointer to the duplicated string */
 char *_strdup(const char *str)
 {
 	int length = 0;
@@ -37,11 +43,14 @@ char *_strdup(const char *str)
 		return (NULL);
 	while (*str++)
 		length++;
+	/* Allocate memory for the duplicated string */
 	ret = malloc(sizeof(char) * (length + 1));
+	/*  if memory allocation was not successful, return NULL */
 	if (!ret)
 		return (NULL);
 	for (length++; length--;)
 		ret[length] = *--str;
+	/* return a pointer to the duplicated string */
 	return (ret);
 }
 
@@ -51,12 +60,15 @@ char *_strdup(const char *str)
  *
  * Return: Nothing
  */
+/* function to print a string to the standard output */
 void _puts(char *str)
 {
 	int i = 0;
 
+	/* if the input string is NULL, return without doing anthing */
 	if (!str)
 		return;
+	/* Iterate through the string until there is a null terminator */
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
@@ -71,17 +83,24 @@ void _puts(char *str)
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+/* function to write a character to the standard output, with buffering */
 int _putchar(char c)
 {
+	/* static variable to keep track of the current position in the buffer */
 	static int i;
+	/* static buffer to temporarily store characters before writing to the standard output */
 	static char buf[WRITE_BUF_SIZE];
 
+	/* if the character is a buffer flush signal or if the buffer is full */
 	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
+		/* write the contents of the buffer to the standard output */
 		write(1, buf, i);
-		i = 0;
+		i = 0; /* Reset the buffer position */
 	}
+	/* if the character is not a buffer flush signal */
 	if (c != BUF_FLUSH)
+		/* add the character to the buffer and increment the buffer position */
 		buf[i++] = c;
-	return (1);
+	return (1); /* return 1 for success */
 }
